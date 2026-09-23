4cc1574412a08a5e34e39d3c9fd70e1d2414f6fe
XGamingTechnology <77449751+XGamingTechnology@users.noreply.github.com>
2026-09-23 15:16:27 +0700

    fix: use MapLibre namespace import for v6 typings

--- apps/web/src/components/landing-map-demo.tsx
+++ apps/web/src/components/landing-map-demo.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-import maplibregl, { type Map as MapLibreMap } from "maplibre-gl";
+import * as maplibregl from "maplibre-gl";
+import type { Map as MapLibreMap } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import styles from "./landing-map-demo.module.css";
 
